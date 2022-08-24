0628907e79f99a0110139d1d2570adf0532d2ead
tommy <tcatty7@gmail.com>
2022-08-24 13:34:55 -0400

    f

--- index.js
+++ index.js
@@ -1,112 +1,31 @@
-(function(){
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
+const btn = document.querySelector('.talk');
+
+function speak(sentence) {
+  const text_speak = new SpeechSynthesisUtterance(sentence);
+
+  text_speak.rate = 1
+  text_speak.pitch = 1;
+
+  window.speechSynthesis.speak(text_speak)
+}
+function wishMe() {
+  var day = new Date();
+  var hr = day.getHours();
+
+  if(hr >= 0 && hr < 12) {
+    speak("good morning")
+  }
+
+  else if(hr > 0 && hr <= 17) {
+    speak("good afternoon")
+  }
   
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
-          );
-        }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-  
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-      {
-        question: "what line of code starts every website?",
-        answers: {
-          a: "!DOCTYPE html",
-          b: "body",
-          c: "html"
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "what code aligns an object to the center in html?",
-        answers: {
-          a: "position:center;",
-          b: "move to center",
-          c: "align=center"
-        },
-        correctAnswer: "c"
-      },
-      {
-        question: "how do you make a comment in javascript",
-        answers: {
-          a: "g",
-          b: "comment=g",
-          c: "/g/",
-          d: "/*g*/"
-        },
-        correctAnswer: "d"
-      }
-    ];
-  
-    // Kick things off
-    buildQuiz();
-  
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
-  })();
+ else  {
+    speak("good evening")
+  }
+}
+
+document.body.addEventListener('load', ()=>{
+  speak("hello i am vrcat");
+  wishMe();
+})
